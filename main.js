047a0ec07502fe23954f1d4f5ccf8305a12caa2f
jumadelu <mail@elvis-jekic.com>
2023-12-16 19:36:59 +0100

    Farben wechseln

--- main.js
+++ main.js
@@ -11,14 +11,15 @@ let ctx = canvas.getContext('2d');
 let posX = 50;
 let posY = 50;
 let radius = 30;
-let innerColor = "Red";
+let circleColor = "Red";
 let speedX = 5;
+let speedY = 5;
 
 
 function drawCircle() {
     ctx.beginPath();
     ctx.arc(posX, posY, radius, 0, Math.PI * 2, false)
-    ctx.strokeStyle = innerColor;
+    ctx.strokeStyle = circleColor;
     ctx.stroke();
 }
 
@@ -26,11 +27,17 @@ function drawCircle() {
 function draw() {
     window.requestAnimationFrame(draw);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCircle(posX, posY, radius, innerColor);
+    drawCircle(posX, posY, radius, circleColor);
     if (posX + radius > canvas.width || posX - radius < 0) {
         speedX = -speedX;
+        circleColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    }
+    if (posY + radius > canvas.height || posY - radius < 0) {
+        speedY = -speedY;
+        circleColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     }
     posX += speedX;
+    posY += speedY;
 }
 
 draw();
